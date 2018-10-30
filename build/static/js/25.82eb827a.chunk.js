932b2fc42361970192c97db733d687ddb0897292
giorgi <giorgi.bibilashvili89@gmail.com>
2018-10-30 22:59:46 +0400

    --

---
+++ build/static/js/25.82eb827a.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{533:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return d});var a=t(6),c=t(7),r=t(10),u=t(8),i=t(9),o=t(1),s=t.n(o),d=function(e){function n(){return Object(a.a)(this,n),Object(r.a)(this,Object(u.a)(n).apply(this,arguments))}return Object(i.a)(n,e),Object(c.a)(n,[{key:"render",value:function(){return s.a.createElement("div",{className:"animated fadeIn"},s.a.createElement("h1",null,"BACKUPS"))}}]),n}(o.Component)}}]);
+//# sourceMappingURL=25.82eb827a.chunk.js.map
