59d0576926ba9d45d36c1aa2317f81aecb592707
giorgi <giorgi.bibilashvili89@gmail.com>
2018-10-30 23:14:54 +0400

    --

--- src/views/Backups/Backups.js
+++ src/views/Backups/Backups.js
@@ -4,7 +4,7 @@ export default class Backups extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        <h1>BACKUPS</h1>
+        <h1>BACKUPS LIST</h1>
       </div>
     )
   }
